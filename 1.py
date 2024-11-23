45aeb6b864fcffe2adcce8939e17923b6391126f
Max Bakhtin <maximbakhtin83@gmail.com>
2024-11-23 11:14:29 +0300

    новый коммит-добавили код в 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,11 @@
 print("devops411")
+str1="Ggg"
+int1=5
+float1=7.
+float2=7.1
+list1=[1,2,3,4,5]
+dict1={'Name':"Ivan","Second Name":"Ivanov","BirtthDate":"25/05/1970"}
+tuple1=(1,2,3,4,5)
+set1={1,2,3,4}
+print(type(set1))
+
